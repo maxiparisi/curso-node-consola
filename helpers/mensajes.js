305e28490986385af016f9879826e6f2acf34f20
Maximiliano Parisi <maximiliano.parisi@gire.com>
2022-03-12 17:17:05 -0300

    primer commit de app de consola hecha en node

---
+++ helpers/mensajes.js
@@ -0,0 +1,49 @@
+const { rejects } = require('assert');
+const { resolve } = require('path');
+
+require('colors');
+
+
+const mostrarMenu = () => {
+
+    return new Promise((resolve, reject) => {
+        console.clear();
+        console.log('==============================='.green);
+        console.log('     Seleccione una opcion'.green);
+        console.log('===============================\n'.green);
+    
+        console.log(`${ '1.'.green } Crear tarea`);
+        console.log(`${ '2.'.green } Listar tareas`);
+        console.log(`${ '3.'.green } Listar tareas completadas`);
+        console.log(`${ '4.'.green } Listar tareas pendientes`);
+        console.log(`${ '5.'.green } Completar tarea`);
+        console.log(`${ '6.'.green } Borrar tarea`);
+        console.log(`${ '0.'.green } Salir\n`);
+    
+        const readline = require('readline').createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+    
+        readline.question('Seleccione una opción: ', (opt) => {
+            readline.close();
+            resolve(opt);
+        })
+    })
+}
+
+const pausa = () => {
+     return new Promise ((resolve, reject) => {
+        const readline = require('readline').createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+    
+        readline.question(`Presione ${'ENTER'.green} para continuar\n`, (opt) => {
+            readline.close();
+            resolve(); //no retorno el opt porqu eno me interesa q tecla presiono
+        })
+     })
+}
+
+module.exports = {mostrarMenu, pausa}
